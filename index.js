a77d999ac3deec927f372061da3667a5d378fb85
Alfredo Vanegas <thealf154@gmail.com>
2020-10-09 16:58:01 -0500

    Added the pokdex file and a way to dinamically search fro them. Also the nodemon is a default command in npm start

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 //Load dependencies
 const express = require("express");
 const app = express();
+const { pokemon }= require("./pokedex.json");
 
-/*
-    Verbos HTTP:
-    Son maneras que puedes obtener recursos de internet:
-    GET: Obtener un recurso. Usado comunmente en navegadores.
-    POST: Guardar o publicar algo en un servidor web.
-    PATCH: A la actualización de un recurso específico.
-    PUT: Modificar todos los elementos
-    DELETE: Eliminar un recurso.
-*/
-
-//Ejemplo de GET
-//La diagonal nos sirve para obtener la url local: localhost:3000/
-// Req es la petición que nos hace el cliente
-// Res es la respuesta
 app.get("/", (req, res, next) => {
-    res.send("Hola Mundo");
     res.status(200); //Todo corrió de manera correcta
+    res.send("Bienvenido a la Pokedex");
 }); //get(url, function)
 
+app.get("/pokemon", (req, res, next) => {
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get("/pokemon/:id", (req, res, next) => {
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
+});
+
 //Load a local server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
