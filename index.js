d882b26ccaecae5d4a2e91c6e85c2bdcb97f23b9
Alfredo Vanegas <thealf154@gmail.com>
2020-10-09 20:09:39 -0500

    Added routes specific for entries of numbers and letters

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 //Load dependencies
 const express = require("express");
 const app = express();
-const { pokemon }= require("./pokedex.json");
+const { pokemon } = require("./pokedex.json");
 
 app.get("/", (req, res, next) => {
-    res.status(200); //Todo corrió de manera correcta
-    res.send("Bienvenido a la Pokedex");
+  res.status(200); //Todo corrió de manera correcta
+  res.send("Bienvenido a la Pokedex");
 }); //get(url, function)
 
-app.get("/pokemon", (req, res, next) => {
-    res.status(200);
-    res.send(pokemon);
+app.get("/pokemon/all", (req, res, next) => {
+  res.status(200);
+  res.send(pokemon);
 });
 
-app.get("/pokemon/:id", (req, res, next) => {
+//Expresión regular que acepta un grupo de tres números
+app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
+  const id = req.params.id - 1;
+  if (id >= 0 && id < 150) {
     res.status(200);
-    res.send(pokemon[req.params.id - 1]);
+    return res.send(pokemon[req.params.id - 1]);
+  }
+  res.status(404);
+  res.send("Pokemón no enctontrado");
+});
+
+app.get("/pokemon/:name", (req, res, next) => {
+  const name = req.params.name;
+  for (i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].name === name) {
+      res.status(200);
+      res.send(pokemon[i]);
+    }
+  }
+  //En caso que no encuentré pokémon, se ejecuta el error
+  res.status(404);
+  res.send("Pokémon no encontrado");
 });
 
 //Load a local server
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running...");
+  console.log("Server is running...");
 });
