3663f238217f27bd210e0b8a9079dbdb8b2ab263
Alfredo Vanegas <thealf154@gmail.com>
2020-10-12 22:23:36 -0500

    Added a route for POST, bodyParser module, and a way to get pokemon's names efficiently

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 //Load dependencies
-const express = require("express");
+const bodyParser = require("body-parser");
+const express = require('express');
 const app = express();
 const { pokemon } = require("./pokedex.json");
 
+//Required to interact with POSTMAN
+//"use" will put a function for every petition to the server 
+//Known better as a middleWare
+app.use(bodyParser.json()); //For every petition it will be passed to a json
+app.use(bodyParser.urlencoded({extended: true})); //This will put the json as a urlenconded
+
 app.get("/", (req, res, next) => {
-  res.status(200); //Todo corrió de manera correcta
-  res.send("Bienvenido a la Pokedex");
+  return res.status(200).send("Bienvenido a la Pokedex"); //También es válido
 }); //get(url, function)
 
-app.get("/pokemon/all", (req, res, next) => {
-  res.status(200);
-  res.send(pokemon);
+app.get("/pokemon", (req, res, next) => {
+  return res.send(pokemon).status(200);
+});
+
+//This route can't be accessed by normal browsers since they can only do GET requests
+app.post("/pokemon", (req, res, next) => {
+  return res.status(200).send(req.body);
 });
 
 //Expresión regular que acepta un grupo de tres números
@@ -21,20 +31,18 @@ app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
     return res.send(pokemon[req.params.id - 1]);
   }
   res.status(404);
-  res.send("Pokemón no enctontrado");
+  return res.send("Pokemón no enctontrado");
 });
 
-app.get("/pokemon/:name", (req, res, next) => {
+//This only receives text requests
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
   const name = req.params.name;
-  for (i = 0; i < pokemon.length; i++) {
-    if (pokemon[i].name === name) {
-      res.status(200);
-      res.send(pokemon[i]);
-    }
-  }
-  //En caso que no encuentré pokémon, se ejecuta el error
-  res.status(404);
-  res.send("Pokémon no encontrado");
+  //Return the pokemon that the user wants
+  const pk = pokemon.filter((p) => {
+    return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+  });
+  //If it didn't find th pokemon, we return a 404 error
+  pk.length > 0 ? res.status(200).send(pk) : res.status(404).send("Pokémon no encontrado");
 });
 
 //Load a local server
