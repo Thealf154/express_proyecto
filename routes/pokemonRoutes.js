4a5cb3688032b01e849ea587a1536ee3e83199a5
Alfredo Vanegas <thealf154@gmail.com>
2020-10-21 20:02:00 -0500

    Added a way to add pokemon to a database

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -1,5 +1,5 @@
-const { json } = require('body-parser');
-const express = require('express');
+const { json } = require("body-parser");
+const express = require("express");
 const pokemonRoutes = express.Router();
 const db = require("../config/database");
 
@@ -9,30 +9,52 @@ pokemonRoutes.get("/", async (req, res, next) => {
   const pkmn = await db.query("SELECT * FROM pokemon");
   console.log(pkmn);
   //Sends the database as a json, but only if the format is already in a json
-  return res.status(200).json({code: 1, message: pkmn});
+  return res.status(200).json({ code: 1, message: pkmn });
 });
 
 //This route can't be accessed by normal browsers since they can only do GET requests
-pokemonRoutes.post("/", (req, res, next) => {
-  return res.status(200).send(req.body);
+pokemonRoutes.post("/", async (req, res, next) => {
+  const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+  if (pok_name && pok_height && pok_base_experience && pok_weight) {
+    let query =
+      "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+    query += ` VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+    const rows = await db.query(query);
+    if (rows.affectedRows == 1) {
+      return res
+        .status(200)
+        .json({ code: 201, message: "Pokemon insertado correctamente" });
+    }
+    return res
+      .status(500)
+      .json({ code: 500, message: "Campos incompletos"});
+  }
+  return res
+    .status(500)
+    .json({ code: 500, message: "Pokemon insertado sin éxito" });
 });
 
 //Expresión regular que acepta un grupo de tres números
-pokemonRoutes.get("/:id([0-9]{1,3})", async(req, res, next) => {
+pokemonRoutes.get("/:id([0-9]{1,3})", async (req, res, next) => {
   const pokemon = await db.query("SELECT * FROM pokemon");
   const id = req.params.id - 1;
   if (id >= 0 && id < 722) {
-    res.status(200).json({ code: 1, message: pokemon[id]});
+    return res.status(200).json({ code: 200, message: pokemon[id] });
   }
-  return res.status(404).json({code: 404, message: "Pokemon no encontrado"}); 
+  return res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
 });
 
 //This only receives text requests
 pokemonRoutes.get("/:name([A-Za-z]+)", async (req, res, next) => {
   const name = req.params.name;
-  let pokemon = await db.query(`SELECT pok_name FROM pokemon WHERE pok_name LIKE '%${name}%'`);
-  if(pokemon.length > 0)
-    return res.status(404).json({code: 404, message: "Pokemon no encontrado"}); 
+  let pokemon = await db.query(
+    `SELECT pok_name FROM pokemon WHERE pok_name LIKE '%${name}%'`
+  );
+  if (pokemon.length > 0)
+    return res
+      .status(404)
+      .json({ code: 404, message: "Pokemon no encontrado" });
   return res.status(200).send(pokemon);
 });
 
