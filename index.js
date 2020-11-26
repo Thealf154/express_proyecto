7bb402c70a706f465dcf9496016662bf890aace6
Alfredo Vanegas <thealf154@gmail.com>
2020-11-25 18:51:56 -0600

    Replaced npm with node

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.use(express.urlencoded({ extended: true}));
 
 
 //This is to manage the user database requests
-app.get("/", (req, res, next) => {
-  res.status(200);
-  res.send("Hola mundo");
-});
+app.get("/", index);
 app.use("/user", user);
 app.use(auth);
 //This is a reference to all the routes in pokemonRoutes
